defb65090ca2e71adfe756560a0be287fb34b4d3
Digvijay010606 <digvijay010606@gmail.com>
2025-03-01 13:07:41 +0530

    Added comments in the code for visitors

--- MadLibs/madlibs.py
+++ MadLibs/madlibs.py
@@ -1,30 +1,36 @@
+# importing libraries(streamlit)
 import streamlit as st
 
+# setting up page configurations
 st.set_page_config(
     page_title="MadLibs",
     page_icon="icon.jpeg"
     )
 
-
+# adding title with a image
 st.title("Welcome to")
 st.image("logo.png")
 
 
-
+# taking inputs from the user
 noun = st.text_input("Noun",placeholder="Ex:-girl")
 verb = st.text_input("Verb",placeholder="Ex:-visit")
 adjective = st.text_input("Adjective",placeholder="beautiful")
 place = st.text_input("Place",placeholder="boston")
 
-
+# story of the madlib
 story = f"Once upon a time, there was a {adjective} {noun} who loved to {verb} in {place}"
 print(story)
-    
+
+# defining container for the output    
 container = st.container(border=True)
+
+# button for generating madlib
 if st.button("Generate",type="secondary"):
     container.success(story)
 else:
     container.write("The MadLib will display here")
-    
+
+# reset button     
 st.button("Reset",type="primary")
 
